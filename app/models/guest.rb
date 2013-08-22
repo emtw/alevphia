f8382ab9cfd45996f73e93138cea0e607cf52809
Emily Twist <emily.twist@btinternet.com>
2013-08-22 12:40:44 +0100

    Guest validations added

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -7,4 +7,14 @@ class Guest < ActiveRecord::Base
   attr_accessible :guest, :name, :user_id, :username, :password, :password_confirmation
   
   belongs_to :user
+  
+  validates :username, :length => { :minimum => 4, :maximum => 30 }, :presence => true, :uniqueness => true, :username_format => true
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :password_format => true,
+                       :on => :create
+  validates :password, :confirmation => true,
+                       :password_format => true,
+                       :allow_blank => true,
+                       :on => :update
 end
