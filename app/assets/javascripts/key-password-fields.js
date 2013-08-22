8d549ab28c409725e4083a18c20722c3a5ae4b5a
Emily Twist <emily.twist@btinternet.com>
2013-08-22 02:13:10 +0100

    All forms updated and UI similar throughout

---
+++ app/assets/javascripts/key-password-fields.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('#key_pw_btn').click(function(){
+		$('#key_pw_field').hide(function(){
+			$('#key_pw').slideDown(200);	
+		});
+	});
+});
---
+++ config/initializers/my_constants.rb
@@ -0,0 +1,7 @@
+COUNTIES = ['Avon', 'Bedfordshire', 'Berkshire', 'Buckinghamshire', 'Cambridgeshire', 'Cheshire', 'Cleveland', 'Cornwall', 
+            'County Durham', 'Cumbria', 'Derbyshire', 'Devon', 'Dorset', 'East Sussex', 'Essex', 'Gloucestershire', 
+             'Hampshire', 'Herefordshire', 'Hertfordshire', 'Isle of Wight', 'Kent', 'Lancashire', 'Leicestershire', 
+             'Lincolnshire', 'London', 'Merseyside', 'Norfolk', 'North Yorkshire', 'Northamptonshire', 'Northumberland', 
+             'Nottinghamshire', 'Oxfordshire', 'Shropshire', 'Somerset', 'South Yorkshire', 'Staffordshire', 'Suffolk', 
+             'Surrey', 'Tyne and Wear', 'Warwickshire', 'West Midlands', 'West Sussex', 'West Yorkshire', 
+             'Wiltshire', 'Worcestershire']
